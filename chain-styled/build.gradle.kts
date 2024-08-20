232baef5d79779cf2ad8f582be9f9231ce1f5ec1
Cdm2883 <Cdm2883@hotmail.com>
2024-08-20 22:25:14 +0800

    build: support all targets

--- chain-styled/build.gradle.kts
+++ chain-styled/build.gradle.kts
@@ -21,9 +21,24 @@ kotlin {
     }
     macosX64()
     macosArm64()
+    iosSimulatorArm64()
+    iosX64()
     linuxX64()
     linuxArm64()
+    watchosSimulatorArm64()
+    watchosX64()
+    watchosArm32()
+    watchosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    tvosArm64()
+    iosArm64()
+    androidNativeArm32()
+    androidNativeArm64()
+    androidNativeX86()
+    androidNativeX64()
     mingwX64()
+    watchosDeviceArm64()
 
     sourceSets {
         val commonMain by getting {
